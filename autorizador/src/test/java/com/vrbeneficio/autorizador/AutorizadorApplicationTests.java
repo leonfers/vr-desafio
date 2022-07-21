3f1e291a2bd7e14906540f84d1dbd0edda40631d
lyonfers <lyonfers@pop-os.localdomain>
2022-07-21 16:22:21 -0300

    inicializando projeto com banco de dados e dependencias

---
+++ autorizador/src/test/java/com/vrbeneficio/autorizador/AutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrbeneficio.autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
